c202f44097882a496baa8c614864084a505b9101
Ash45# <ashishsahoo2015@gmail.com>
2023-02-09 17:34:44 +0530

    3rd commit

--- pages/create.js
+++ pages/create.js
@@ -3,7 +3,7 @@ import { isAddress, parseEther } from "ethers/lib/utils";
 import Link from "next/link";
 import { useState } from "react";
 import { useSigner, erc721ABI } from "wagmi";
-import MarketplaceABI from "../abis/NFTMarketplace.json";
+// import MarketplaceABI from "../abis/NFTMarketplace.json";
 import Navbar from "../components/Navbar";
 import styles from "../styles/Create.module.css";
 // import { MARKETPLACE_ADDRESS } from "../constants";
